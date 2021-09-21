631d45290f12b35437be29466f996ec27808dd2e
leilamyar <leilamyar.dev@gmail.com>
2021-09-21 16:02:53 +0200

    fix(about-gallery) : layout

--- views/pages/about.php
+++ views/pages/about.php
@@ -8,4 +8,13 @@
     <img src="public/images/phinehas-adams-unsplash.jpg" alt="" srcset="">
     <img src="public/images/rae-goldman-unsplash.jpg" alt="" srcset="">
     <img src="public/images/zahra-tavakoli-fard-unsplash.jpg" alt="" srcset="">
+    <img src="public/images/brooke-lark-unsplash.jpg" alt="" srcset="">
+    <img src="public/images/bryam-blanco-unsplash.jpg" alt="" srcset="">
+    <img src="public/images/caitlyn-de-wild-unsplash.jpg" alt="" srcset="">
+    <img src="public/images/david-holifield-unsplash.jpg" alt="" srcset="">
+    <img src="public/images/deva-williamson-unsplash.jpg" alt="" srcset="">
+    <img src="public/images/jacob-schwartz-unsplash.jpg" alt="" srcset="">
+    <img src="public/images/phinehas-adams-unsplash.jpg" alt="" srcset="">
+    <img src="public/images/rae-goldman-unsplash.jpg" alt="" srcset="">
+    <img src="public/images/zahra-tavakoli-fard-unsplash.jpg" alt="" srcset="">
 </div>
